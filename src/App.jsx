620a7a73a7d61dc02373b33a5f74e97b5c1d1fd6
“imtiazins” <imtiaz3996@gmail.com>
2023-09-17 02:47:03 +0600

    explore props basic

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,24 @@ function App() {
   return (
     <>
       <h1>Vite + React</h1>
+      
+      <Device name="Laptop"  price="55k"></Device>
+      <Device name="Mobile" price = "19k" ></Device>
+      <Device name="watch" price = "3k" ></Device>
+
       <Person></Person>
-      <Person></Person>
+      <Student grade="7" score="99"></Student>
       <Student></Student>
+      <Student></Student>
+      <Developer></Developer>
+      
     </>
   )
 }
-
+function Device(props){
+  // console.log(props);
+  return <h2>This is device: {props.name} , price: {props.price}</h2>
+}
 
 function Person (){
   const age = 23;
@@ -23,13 +34,32 @@ function Person (){
   return <h3>I am {person.name} with age : {age} & I have only {money} tk in my pocket</h3>
 }
 
-function Student (){
+
+const {grade, score} ={grade:"7", score:"99"}
+function Student (props){
+  console.log(props);
   return (
-  <div>
+  <div className='student'>
     <h3>This is a student</h3>
     <p>Name:</p>
     <p>age:</p>
   </div>
+  )
+}
+
+function Developer(){
+  const developerStyle={
+    margin:'20px',
+    padding:'10px',
+    border:'2px solid purple',
+    borderRadius:'20px'
+
+  }
+  return(
+    <div style={developerStyle}>
+      <h5>Devo Devo</h5>
+      <p>coding:</p>
+    </div>
   )
 }
 
